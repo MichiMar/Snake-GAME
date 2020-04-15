3419ddfecbf9ce363ad1d8f700e95025ce7561bb
MichiMar <michimar.alexis@gmail.com>
2020-04-15 16:18:51 -0500

    update functions and homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef, useReducer } from "react";
+import "./App.css";
+
+function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    useEffect( () => {
+      savedCallback.current = callback;
+    })
+
+    useEffect(() => {
+      function tick() {
+        savedCallback.current()
+      }
+
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id)
+    }, [delay]
+    );
+}
+
+
+// Timer Component
+function Timer({pause}) {
+  const [hour, setHours] = useState(0);
+  const [minute, setMinutes] = useState(0);
+  const [second, setSeconds] = useState(0);
+
+  const toTime = (time) => ("0" + time).slice(-2);
+
+  let resetRef = useRef();
+  // Trick to initialize countRef.current on first render only
+  resetRef.current = resetRef.current || false;
+
+  useEffect(() => {
+    if (resetRef.current === true) {
+      setSeconds(0)
+    }
+  })
+
+  useInterval(() => {
+    if (pause) {
+      resetRef.current = true;
+      return;
+    }
+  })
+}
+
+
 
 function App() {
+
+  
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header />
     </div>
   );
 }
